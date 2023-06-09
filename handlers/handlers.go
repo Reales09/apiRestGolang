2a62949896b6cc0baba12fbfae323adced11ec9c
Reales <rclarence123@gmail.com>
2023-06-08 22:12:19 -0500

    Se agrega la opción de actualizar y eliminar

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -38,13 +38,54 @@ func GetUser(rw http.ResponseWriter, r *http.Request) {
 }
 
 func CreateUser(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "Crea un usuario")
+	rw.Header().Set("Content-Type", "application/json")
+	// rw.Header().Set("Content-Type", "test/xml")
+
+	//Obtener registro
+	user := models.User{}
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&user); err != nil {
+		fmt.Fprintln(rw, http.StatusUnprocessableEntity)
+	} else {
+		db.Connect()
+		user.Save()
+		db.Close()
+	}
+	output, _ := json.Marshal(user)
+	fmt.Fprintln(rw, string(output))
+
 }
 
 func UpdateUser(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "Actualiza un usuario")
+	rw.Header().Set("Content-Type", "application/json")
+	// rw.Header().Set("Content-Type", "test/xml")
+
+	//Actualizar un registro
+	user := models.User{}
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&user); err != nil {
+		fmt.Fprintln(rw, http.StatusUnprocessableEntity)
+	} else {
+		db.Connect()
+		user.Save()
+		db.Close()
+	}
+	output, _ := json.Marshal(user)
+	fmt.Fprintln(rw, string(output))
 }
 
 func DeleteUser(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "Elimina un usuario")
+	rw.Header().Set("Content-Type", "application/json")
+	// rw.Header().Set("Content-Type", "test/xml")
+
+	//Eliminar un registro
+	vars := mux.Vars(r)
+	userId, _ := strconv.Atoi(vars["id"])
+
+	db.Connect()
+	user := models.GetUser(userId)
+	user.Delete()
+	db.Close()
+	output, _ := json.Marshal(user)
+	fmt.Fprintln(rw, string(output))
 }
